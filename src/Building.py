1d563b86a176d53051285c79a1c44e2f7f67dfb5
Eric Xue <eric.xue@hotmail.com>
2022-10-22 09:25:00 -0700

    Fix closure-based bug and add water wheel and coal mine.

--- src/Building.py
+++ src/Building.py
@@ -12,4 +12,6 @@ class Building:
 
 BUILDINGS = (
     Building('campfire', 'Campfire', Decimal(15), Decimal(0.2)),
+    Building('water-wheel', 'Water Wheel', Decimal(130), Decimal(2)),
+    Building('coal-mine', 'Coal Mine', Decimal(1.17e3), Decimal(16)),
 )
